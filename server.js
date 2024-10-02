8c6308ad18d16a67fa1dc31eb727d9631b14587a
iury almeida <iury.colombo@gmail.com>
2024-10-01 22:07:52 -0400

    asd

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-console.log()
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+let ledState = false; // Estado do LED
+
+app.use(express.json());
+
+app.post('/led/on', (req, res) => {
+    ledState = true;
+    console.log("LED ligado");
+    res.send('LED ligado');
+});
+
+app.post('/led/off', (req, res) => {
+    ledState = false;
+    console.log("LED desligado");
+    res.send('LED desligado');
+});
+
+app.get('/led/state', (req, res) => {
+    res.json({ state: ledState });
+});
+
+app.listen(port, () => {
+    console.log(`API rodando na porta ${port}`);
+});
+
+
+//hello
